4e896473626fee7cdacb47d9b59a1efb750e31b4
ianis-dev <ianischennaf@outlook.com>
2023-06-20 15:51:38 +0200

    page main & début page profil

--- app/src/main/java/com/example/gomuscuapp/MainActivity.java
+++ app/src/main/java/com/example/gomuscuapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gomuscuapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -53,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button goToTest;
 
-        goToTest = findViewById(R.id.buttonTestApi);
+        /*goToTest = findViewById(R.id.buttonTestApi);
         goToTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,6 +62,8 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, TestApi.class);
                 startActivity(intent);
             }
-        });
+        });*/
+
+
     }
 }
