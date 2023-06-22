2e49cac901c234caee5c66fdf175a77ba599618e
ianis-dev <ianischennaf@outlook.com>
2023-06-22 10:00:39 +0200

    Ajout detail activity

---
+++ app/src/main/java/com/example/gomuscuapp/Activity_details.java
@@ -0,0 +1,14 @@
+package com.example.gomuscuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
