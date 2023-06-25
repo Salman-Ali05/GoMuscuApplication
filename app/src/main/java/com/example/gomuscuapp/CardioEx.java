4099f4a11ddf7517fd6d1f77c30a436b92f7751a
ManissaNina <manissagym@gmail.com>
2023-06-25 15:42:20 +0200

    color changes + variables + setup Cardio Class

---
+++ app/src/main/java/com/example/gomuscuapp/CardioEx.java
@@ -0,0 +1,58 @@
+package com.example.gomuscuapp;
+
+public class CardioEx {
+    private String name;
+    private String type;
+    private String muscle;
+    private String equipment;
+    private String difficulty;
+    private String instructions;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMuscle() {
+        return muscle;
+    }
+
+    public void setMuscle(String muscle) {
+        this.muscle = muscle;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(String equipment) {
+        this.equipment = equipment;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+}
